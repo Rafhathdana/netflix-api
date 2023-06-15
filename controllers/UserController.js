0926833172b5f8b9c09506a5d2a4a6499934a6e4
MOHAMMED RAFHATH <danarafha@gmail.com>
2023-06-15 17:31:32 +0530

    don ecoding for adding user with liked videos

---
+++ controllers/UserController.js
@@ -0,0 +1,28 @@
+const User = require("../models/UserModel");
+
+module.exports.addToLikedMovies = async (req, res) => {
+  try {
+    const { email, data } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      const { likedMovies } = user;
+      const movieAlreadyliked = likedMovies.find(({ id }) => {
+        id = data.id;
+      });
+      if (!movieAlreadyliked) {
+        await User.findByIdAndUpdate(
+          user._id,
+          {
+            likedMovies: [...user.likedMovies, data],
+          },
+          {
+            new: true,
+          }
+        );
+      } else return res.json({ msg: "Movie already added to the liked list." });
+    } else await User.create({ email, likedMovies: [data] });
+    return res.json({ msg: "Movie added successfully" });
+  } catch (error) {
+    return res.json({ msg: "Error adding movie" });
+  }
+};
