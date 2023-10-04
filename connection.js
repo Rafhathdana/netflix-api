aa176a8f630351cab60accca77359559879bab03
MOHAMMED RAFHATH <danarafha@gmail.com>
2023-10-04 16:27:24 +0530

    done some updation

---
+++ connection.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    console.log(process.env.MONGODB_URI, "hello");
+    await mongoose
+      .connect(`${process.env.MONGODB_URI}`)
+      .then((e) =>
+        console.log(`connected to database - ${e.connections[0].name}`)
+      );
+  } catch (err) {
+    console.log(err, process.env.MONGODB_URI);
+  }
+};
+
+// if (process.env.FAKE_BUCKET) console.log("Faking S3 Bucket");
+// else console.log("\nS3 Bucket is active");
+// console.log("Enviornment is ", process.env.NODE_ENV);
+mongoose.connection
+  .on("open", async () => console.log("Mongoose connected successfully"))
+  .on("error", async (err) => console.error("mongoose connection error", err));
+mongoose.connection.on("disconnected", () => {
+  console.log("mongodb disconnected");
+});
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,17 +1,42 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/UserRoutes");
+const connectDB = require("./connection");
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose
-  .connect("mongodb://localhost:27017/netflix", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connected");
-  });
+// mongoose
+//   .connect("mongodb://localhost:27017/netflix", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("DB Connected");
+//   });
 app.use("/api/user", userRoutes);
-app.listen(5000, console.log("server started"));
+
+const server = app.listen(process.env.PORT, () => {
+  connectDB();
+  console.log(`server started at port${process.env.PORT}`);
+});
+//process closed due to the uncaught exception and exit with code of 1 (indicating an error)
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught exception - ", err);
+  console.log("closing node process");
+  process.exit(1);
+});
+//This event is triggered when a Promise is rejected, but there is no .catch() or .then(null, ...) handler to handle the rejection.
+process.on("unhandledRejection", (err) => {
+  console.log("unhandled rejection - ", err);
+  console.log("closing node process");
+  process.exit(1);
+});
+//The server is closed by invoking its close method, which stops it from accepting new connections. The server will continue processing existing connections until they are completed or closed by the clients.
+process.on("SIGTERM", () => {
+  server.close((err) => {
+    console.log("Http server closed");
+    process.exit(err ? 1 : 0);
+  });
+});
